6ac70d9de866d7653d1bd61242386531f85c01cb
pravekotian123 <47718965+pravekotian123@users.noreply.github.com>
2024-01-25 07:53:09 +0000

    updates

--- assignment_operator/assignment_operator.cpp
+++ assignment_operator/assignment_operator.cpp
@@ -19,10 +19,32 @@ class array
        return this->arr[n];
        
     }
+    template<T> friend array operator+ (array &arr1,array &arr2);
 };
+template <typename T, int s>
+array<T,s+s> operator+ (array<T,s> &arr1,array<T,s> &arr2)
+{
+    int i;
+    array<T,s+s> arr4={99,99,99,99,99,99,99,99,99,99};
+    for( i=0;i<s;i++)
+    {
+        arr4.arr[i] = arr1.arr[i];
+    }
+    for( i=0;i<s;i++)
+    {
+        arr4.arr[s+i] = arr2.arr[i];
+    }
+
+    return arr4;
+}
 int main(void)
 {
-	array<int,5> arr={1,2,3,4,5};
-	std::cout<<arr[4];
+	array<int,5> arr1={1,2,3,4,5};
+    array<int,5> arr2={6,7,8,9,10};
+	array<int,10> arr3=arr1+arr2;
+    for(int i=0;i<10;i++)
+    {
+        cout<<arr3[i]<<" ";
+    }
 	return 0;
 }
