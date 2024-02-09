d0e1a379833385ba1b5d3965c2ef755e63680680
pravekotian123 <47718965+pravekotian123@users.noreply.github.com>
2024-02-09 15:39:03 +0000

    updates

--- assignment_operator/assignment_operator.cpp
+++ assignment_operator/assignment_operator.cpp
@@ -25,13 +25,13 @@ class array
 template <typename T, int s,int t>
 array<T,s+t> operator+ (array<T,s> &arr1,array<T,t> &arr2)
 {
-    int i;
+    
     array<T,s+t> arr4 = {0};
-    for( i=0;i<s;i++)
+    for(int i=0;i<s;i++)
     {
         arr4.arr[i] = arr1.arr[i];
     }
-    for( i=0;i<s;i++)
+    for(int i=0;i<t;i++)
     {
         arr4.arr[s+i] = arr2.arr[i];
     }
@@ -40,9 +40,11 @@ array<T,s+t> operator+ (array<T,s> &arr1,array<T,t> &arr2)
 }
 int main(void)
 {
+    
 	array<int,5> arr1={1,2,3,4,5};
     array<int,4> arr2={6,7,8,9};
 	array<int,9> arr3=arr1+arr2;
+    
     for(int i=0;i<9;i++)
     {
         cout<<arr3[i]<<" ";
